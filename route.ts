030e1d5fa6981b81addd97fc66f80efdb7b1a4c0
camille-sommer <106606900+camille-sommer@users.noreply.github.com>
2022-06-21 14:31:50 -0300

    Update route.ts

--- route.ts
+++ route.ts
@@ -66,3 +66,66 @@ getUserPermission,
 new OrderController().conclude
 );
 
+//? Products
+router.get("/products", getUserPermission, new ProductsController().listAll);
+router.get("/products/:id", new ProductsController().find);
+router.delete(
+"/products/:id",
+ensureAuthenticated,
+getUserPermission,
+new ProductsController().delete
+);
+router.post(
+"/products",
+ensureAuthenticated,
+getUserPermission,
+new ProductsController().create
+);
+router.put(
+"/products/:id",
+ensureAuthenticated,
+getUserPermission,
+new ProductsController().update
+);
+router.post(
+"/products/:id/categories",
+ensureAuthenticated,
+getUserPermission,
+new ProductsController().vinculateCategory
+);
+
+router.post(
+"/products/:id/sizes",
+ensureAuthenticated,
+getUserPermission,
+new ProductsController().vinculateSize
+);
+//? Sizes
+router.get("/sizes", new SizesController().listAll);
+router.delete(
+"/sizes/:id",
+ensureAuthenticated,
+getUserPermission,
+new SizesController().delete
+);
+router.post(
+"/sizes",
+ensureAuthenticated,
+getUserPermission,
+new SizesController().create
+);
+//? Categories
+router.get("/categories/", new CategoriesController().listAll);
+router.delete(
+"/categories/:id",
+ensureAuthenticated,
+getUserPermission,
+new CategoriesController().delete
+);
+router.post(
+"/categories",
+ensureAuthenticated,
+getUserPermission,
+new CategoriesController().create
+);
+export { router };
